b953a308e4dbf47eddb9ae599204426648921fb4
chandinijain <chandinijain206@gmail.com>
2017-01-26 08:16:13 +0530

    Update competition.py

--- competitionToolbox/competition.py
+++ competitionToolbox/competition.py
@@ -5,7 +5,7 @@ import auquanToolbox as at
 
 def runSolution(markets, lookback, trading_strategy,verbose=False):
     budget=1000000
-    base_index='SPX'
+    base_index='INX'
     exchange = 'abcd'
     date_start = '01-01-2010'
     date_end = '31-12-2014'
